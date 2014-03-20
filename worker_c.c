436283f7a39e3e9b880114bea4d6375a2f575f6d
Madison Stemm <astromaddie@gmail.com>
2014-03-20 14:42:19 -0400

    Repaired MPI_Init()

--- worker_c.c
+++ worker_c.c
@@ -5,34 +5,40 @@
 const int nelements1 = 1E3;
 const int nelements2 = 1E6;
 
-int outarray(){
-	float* array = malloc(sizeof(float) * nelements2);
-	int i = 0;
-	for( i = 0; i < nelements2; i++ ){
-		array[i] = 1.110001;
-	}
+//float outarray(){
+//	float* array = malloc(sizeof(float) * nelements2);
+//	int i = 0;
+//	for( i = 0; i < nelements2; i++ ){
+//		array[i] = 1.110001;
+//	}
+//
+//	return *array;
+//}
 
-	return *array;
-}
-
-int main(int argc, char *argv[]){
-
-MPI_Comm comm;
-MPI_Comm_get_parent(&comm);
+int main(argc, argv)
+int argc;
+char *argv[];
+{
 
 int myid, world_size;//, size;
 int root = 0;
 int* endloop = malloc(sizeof(int));
-void* input = malloc(sizeof(float) * nelements1);
-void* output;
+float* input = (float *)malloc(sizeof(float) * nelements1);
+float output[nelements2];
 void* sendbuff;
 int recv = 1;
+int i = 0;
+
+MPI_Init(&argc, &argv);
+MPI_Comm comm;
+MPI_Comm_get_parent(&comm);
 
 MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-output = (void *)outarray();
-
+for( i = 0; i < nelements2; i++ ){
+	output[i] = 1.1100001*i;
+}
 
 //OMPI_DECLSPEC  int MPI_Scatter(void *sendbuf, int sendcount, MPI_Datatype sendtype,
 //                void *recvbuf, int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm);
@@ -47,8 +53,8 @@ while ( endloop[0] < 1) {
 	MPI_Scatter(sendbuff, recv, MPI_INT, endloop, recv, MPI_INT, root, comm);
 }
 
-free(endloop);
-free(input);
+//free(endloop);
+//free(input);
 
 MPI_Finalize();
 }
